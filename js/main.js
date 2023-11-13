2a469ea77c6487c79faacd5a117f5e2fee69313f
Terry RG <jerry794_7@hotmail.com>
2023-11-12 19:38:21 -0500

    Fusión al menu carrita de compras, agregar estilos al archivo principal

--- js/main.js
+++ js/main.js
@@ -1,9 +1,40 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenuEmail = document.querySelector('.desktop-menu');
-menuEmail.addEventListener('click', toggleMenuEmail);
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
+menuEmail.addEventListener('click', toggleMenuEmail);
+menuHamIcon.addEventListener('click', toggleMenuMobil);
+menuCarritoIcon.addEventListener('click', toggleListCarrito);
 
 function toggleMenuEmail(){
+    const isListCarritoClosed = aside.classList.contains('inactive');
+
+    if (!isListCarritoClosed){
+        aside.classList.add('inactive');
+    }
     desktopMenuEmail.classList.toggle('inactive');
 }
 
+function toggleMenuMobil(){
+    const isListCarritoClosed = aside.classList.contains('inactive');
+
+    if (!isListCarritoClosed){
+        aside.classList.add('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleListCarrito(){
+    const isMobilMenuClosed = mobileMenu.classList.contains('inactive');
+    const isEmailMenuClosed = desktopMenuEmail.classList.contains('inactive');
+    if (!isMobilMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    if (!isEmailMenuClosed){
+        desktopMenuEmail.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
+}
