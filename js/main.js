dc31de0885e1327644a4dc099bd33e51ce539d42
Terry RG <jerry794_7@hotmail.com>
2023-11-12 21:19:55 -0500

    Agregar Tienda de Productos creando el html desde JS - array de productos

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleMenuEmail);
 menuHamIcon.addEventListener('click', toggleMenuMobil);
@@ -38,3 +39,76 @@ function toggleListCarrito(){
     }
     aside.classList.toggle('inactive');
 }
+
+const productlist = [];
+productlist.push({
+    name: 'Bicleta Carreras',
+    price: 120,
+    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress',
+});
+productlist.push({
+    name: 'Bike',
+    price: 130,
+    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress',
+});
+productlist.push({
+    name: 'Patineta',
+    price: 420,
+    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress',
+});
+productlist.push({
+    name: 'Bicleta Montain',
+    price: 520,
+    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress',
+});
+productlist.push({
+    name: 'Patines',
+    price: 300,
+    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress',
+});
+productlist.push({
+    name: 'Rodilleras',
+    price: 120,
+    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress',
+});
+
+function detallesDivProductos(arrayProduct){
+    for (product of arrayProduct){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card')
+
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.imagen);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$'+ product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', 'icons/bt_add_to_cart.svg');
+
+        productInfoFigure.appendChild(productImgCart);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+        
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+
+        cardsContainer.appendChild(productCard);
+    }
+
+}
+
+detallesDivProductos(productlist);
+
